89c5db0858c16820eda7dce3ed3b913160c25335
Kendra Kyndberg <kendrak922@gmail.com>
2018-04-03 12:59:42 -0500

    appended train schedule

--- train.js
+++ train.js
@@ -1,4 +1,6 @@
 "use strict";
+
+$(document).ready(function() {
  // Initialize Firebase
  var config = {
     apiKey: "AIzaSyCYbb79whB8H5LSboVJj8QajvFqmLUgpsY",
@@ -12,25 +14,41 @@
 
   const database = firebase.database();
 
+
+  let name = "";
+  let destination = "";
+  let firstTrainTime = 0;
+  let frequency = 0;
+
+
   //Add to database
   $("#submit").on("click", function(event){
     event.preventDefault();
-    let name = $("#train-name-input").val().trim();
-    let destination = $("#destination-input").val().trim();
-    let firstTrainTime = $("#first-train-input").val().trim();
-    let frequency = $("#frequency-input").val().trim();
+     name = $("#train-name-input").val().trim();
+     destination = $("#destination-input").val().trim();
+     firstTrainTime = $("#first-train-input").val().trim();
+     frequency = $("#frequency-input").val().trim();
 
-    $("#train-table").append(`<tr><td>${name}</td><td>${destination}</td><td>${firstTrainTime}</td><td>${frequency}</td></tr>`);
-
-   database.ref().set({
+   let trainSchedule = {
        trainName: name,
         Destination: destination,
         FirstTrain : firstTrainTime,
         Frequency : frequency,
         
+  };
+  database.ref().push(trainSchedule);
+
+ $("#train-name-input").val("");
+ $("#destination-input").val("");
+ $("#first-train-input").val("");
+ $("#frequency-input").val("");
+
   });
 
 
+  database.ref().on("child_added", function(childSnapshot , prevChildKey) {
+
+    $("#train-table").append(`<tr><td>${childSnapshot.val().trainName}</td><td>${childSnapshot.val().Destination}</td><td>${childSnapshot.val().FirstTrain}</td><td>${childSnapshot.val().Frequency}</td></tr>`);
+    console.log(childSnapshot.val());
   });
-
-
+});
